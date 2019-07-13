c6cf9e9b087ca7879fcfe96fe718f006c3cec058
pytaq <1093499975@qq.com>
2019-07-13 17:51:33 +0800

    Spring-mybatis-springmvc

--- src/main/java/cn/gwj/controller/IndexController.java
+++ src/main/java/cn/gwj/controller/IndexController.java
@@ -3,6 +3,7 @@ package cn.gwj.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
@@ -22,10 +23,16 @@ public class IndexController {
 //        return  "login" ;
 //    }
 
+    //http://localhost:8080/bill/add.html?userName=sdfds
     @RequestMapping(value = "add.html",method = RequestMethod.GET,params = "userName")
     public String add(String userName){
         System.out.println("接收到传递来的参数"+userName);
         return "login";
     }
 
+    @RequestMapping("ap.html")
+    public String add(@RequestParam int id){
+        System.out.println("接收到传递来的参数"+id);
+        return "login";
+    }
 }
